7b8cdb4869cb849775c66c6ace12a583b80534c0
Natanzin <natan.silva.judo@gmail.com>
2020-10-29 18:37:57 -0300

    second commit

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+//Arquivo dedicado as configurações de navegação do aplicativo
+const Navigation = () => {
+  return <View />;
+}
+
+export default Navigation;
