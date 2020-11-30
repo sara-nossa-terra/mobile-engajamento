e31e5ac70c5738b4a3fb00d45e2e530139b91dcc
rodrigodiasnoronha <rodrigodnoronha@gmail.com>
2020-11-30 18:09:57 -0300

    feat: layout do menu hamburguer de acordo com o prototipo

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Text, Divider } from 'react-native-paper';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
+import { Feather as Icon } from '@expo/vector-icons';
 import Dashboard from '@pages/Dashboard';
 import ActivityManage from '@pages/Activity';
 import PeopleHelpedManage from '@pages/PeopleHelped';
@@ -10,27 +13,10 @@ import CreateActivity from '@pages/Activity/createActivity';
 import CreatePeopleHelped from '@pages/PeopleHelped/createPeopleHelped';
 import MenuHamburguer from '@components/MenuHamburguer';
 import MenuLogout from '@components/MenuLogOut';
+import { AppColors } from '../types';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
-
-const stackScreenOptions: StackNavigationOptions = {
-  headerTitleAlign: 'center',
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    color: '#fff',
-    fontFamily: 'Montserrat_semi_bold',
-    fontSize: 16,
-  },
-  headerStyle: {
-    backgroundColor: '#3b8ea5',
-    elevation: 0, // remove shadow on Android
-    shadowOpacity: 0, // rem
-  },
-  headerBackTitleStyle: {
-    color: '#fff',
-  },
-};
 
 const DashboardStackNavigator: React.FC = () => (
   <Stack.Navigator screenOptions={stackScreenOptions}>
@@ -117,12 +103,137 @@ const PeopleHelpedNavigator: React.FC = () => (
  *
  */
 const AppNavigator: React.FC = () => (
-  <Drawer.Navigator>
-    <Drawer.Screen name="Dashboard" component={DashboardStackNavigator} options={{ title: 'Dashboard' }} />
-    <Drawer.Screen name="Leader" component={LeaderStackNavigator} options={{ title: 'Líderes' }} />
-    <Drawer.Screen name="Activity" component={ActivityNavigator} options={{ title: 'Atividades' }} />
-    <Drawer.Screen name="PeopleHelped" component={PeopleHelpedNavigator} options={{ title: 'Pessoas Ajudadas' }} />
+  <Drawer.Navigator
+    keyboardDismissMode="on-drag"
+    // estilização do header, footer e content do menu drawer
+    drawerContent={props => (
+      <DrawerContentScrollView {...props} contentContainerStyle={styles.drawerContainer}>
+        <View>
+          <View style={styles.drawerHeader}>
+            <Text style={styles.drawerHeaderText}>Menu</Text>
+          </View>
+          <Divider style={{ marginBottom: 16 }} />
+          <DrawerItemList {...props} />
+        </View>
+
+        <View>
+          <TouchableOpacity onPress={() => {}} activeOpacity={0.1}>
+            <Text style={styles.drawerLogout}>SAIR</Text>
+          </TouchableOpacity>
+        </View>
+      </DrawerContentScrollView>
+    )}
+  >
+    <Drawer.Screen
+      name="Dashboard"
+      component={DashboardStackNavigator}
+      options={{
+        drawerIcon: () => <DrawerLabel title="INICIAL" icon="home" color={AppColors.BLUE} />,
+      }}
+    />
+
+    <Drawer.Screen
+      name="Leader"
+      component={LeaderStackNavigator}
+      options={{
+        drawerIcon: () => <DrawerLabel title="LÍDERES" icon="user" color={AppColors.BLUE} />,
+      }}
+    />
+
+    <Drawer.Screen
+      name="PeopleHelped"
+      component={PeopleHelpedNavigator}
+      options={{
+        drawerIcon: () => <DrawerLabel title="PESSOAS AJUDADAS" icon="users" color={AppColors.BLUE} />,
+      }}
+    />
+
+    <Drawer.Screen
+      name="Activity"
+      component={ActivityNavigator}
+      options={{
+        drawerIcon: () => <DrawerLabel title="ATIVIDADES" icon="plus-square" color={AppColors.BLUE} />,
+      }}
+    />
   </Drawer.Navigator>
+);
+
+const stackScreenOptions: StackNavigationOptions = {
+  headerTitleAlign: 'center',
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    color: '#fff',
+    fontFamily: 'Montserrat_semi_bold',
+    fontSize: 16,
+  },
+  headerStyle: {
+    backgroundColor: '#3b8ea5',
+    elevation: 0, // remove shadow on Android
+    shadowOpacity: 0, // rem
+  },
+  headerBackTitleStyle: {
+    color: '#fff',
+  },
+};
+
+const styles = StyleSheet.create({
+  // header menu drawer
+  drawerHeader: {
+    padding: 17,
+  },
+  drawerHeaderText: {
+    fontSize: 14,
+    fontFamily: 'Montserrat_extra_bold',
+  },
+
+  drawerContainer: {
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+
+  // item menu drawer
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  drawerItemText: {
+    fontFamily: 'Montserrat_extra_bold',
+    fontSize: 12,
+    flex: 1,
+    paddingLeft: 15,
+  },
+
+  // logout
+  drawerLogout: {
+    fontSize: 14,
+    color: AppColors.BLUE,
+    textAlign: 'center',
+    fontFamily: 'Montserrat_extra_bold',
+    padding: 16,
+  },
+});
+
+/**
+ *
+ * Drawer Label
+ *
+ * Label estilado do menu drawer (cada item do menu e tal do drawer)
+ *
+ * title => nome do menu
+ * icon => icone do menu
+ *
+ */
+
+const DrawerLabel: React.FC<{ color: string; focused?: boolean; title: string; icon: string }> = ({
+  color = AppColors.BLUE,
+  title,
+  icon,
+}) => (
+  <View style={styles.drawerItem}>
+    <Icon name={icon} size={20} color={color} />
+    <Text style={[styles.drawerItemText, { color }]}>{title}</Text>
+    <Icon name="chevron-right" size={20} color={color} />
+  </View>
 );
 
 export default AppNavigator;
